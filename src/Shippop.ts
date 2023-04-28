abf5f9ec61883c1f0d5766c8b160aa59c6dbca86
chaya-tan <chayatan.ang@gmail.com>
2023-04-28 14:42:03 +0700

    added booking and confirm purchase method

--- src/Shippop.ts
+++ src/Shippop.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { BookingRequest, BookingResponse } from "@interfaces/book";
+import { BookingDataObject } from "./interfaces/book";
 
 export class Shippop {
   apiKey: string;
@@ -19,9 +20,60 @@ export class Shippop {
   }
 
   // internal API
-  book() {}
-  confirmPurchase() {}
-  cancelOrder() {}
+  async book(
+    bookingDataObjects: BookingDataObject[]
+  ): Promise<{ purchaseId: number; bookingResponseData: BookingResponse }> {
+    try {
+      return await axios<BookingResponse>({
+        method: "post",
+        url: this.bookingEndPoint,
+        data: bookingDataObjects,
+      }).then((bookingResponse) => {
+        const bookingResponseData = bookingResponse.data;
+        const purchaseId = bookingResponse.data.purchase_id;
+        return { bookingResponseData, purchaseId };
+      });
+    } catch (e) {
+      throw new Error(
+        typeof e === "string"
+          ? e
+          : e instanceof Error
+          ? e.message
+          : "Unknown error occurred on booking process"
+      );
+    }
+  }
+
+  async confirmPurchase(purchaseId: number) {
+    try {
+      return axios({
+        method: "post",
+        url: this.confirmEndPoint,
+        data: {
+          api_key: this.apiKey,
+          purchase_id: purchaseId,
+        },
+      });
+    } catch (e) {
+      throw new Error(
+        typeof e === "string"
+          ? e
+          : e instanceof Error
+          ? e.message
+          : "Unknown error occurred on confirm purchase process"
+      );
+    }
+  }
+
+  async bookAndConfirm(
+    bookingDataObjects: BookingDataObject[]
+  ): Promise<BookingResponse> {
+    const { purchaseId, bookingResponseData } = await this.book(
+      bookingDataObjects
+    );
+    await this.confirmPurchase(purchaseId);
+    return bookingResponseData;
+  }
 
   // use cases
   getTrackingData() {}
