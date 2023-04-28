4dd8c4d671fcf8d9ec76dc7fc03f321357f2971f
chaya-tan <chayatan.ang@gmail.com>
2023-04-28 16:09:41 +0700

    v1.0.2 exported types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
 import { Shippop } from "./Shippop";
+import {
+  ParcelObject,
+  AddressObject,
+  ProductObject,
+  PostOfficeObject,
+} from "@interfaces/object";
+import {
+  BookingResponseObject,
+  BookingDataObject,
+  BookingRequest,
+  BookingResponse,
+} from "@interfaces/book";
 
+export {
+  ParcelObject,
+  AddressObject,
+  ProductObject,
+  PostOfficeObject,
+  BookingResponseObject,
+  BookingDataObject,
+  BookingRequest,
+  BookingResponse,
+};
 export default Shippop;
